4e362bb3e8fb81e9f171f50929a38515ebf68df2
ARJUNMASTER16 <anigam525@gmail.com>
2026-07-05 20:42:39 +0000

    updated

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,31 @@
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());           //coordinate universal time (date with time)
+// console.log(myDate.toDateString());       //only date
+// console.log(myDate.toLocaleString());     //date with time with comma separation
+// console.log(typeof myDate);               //date is object
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
