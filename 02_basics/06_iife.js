79aec99a18fb7303ddbbd58c805cf562618828fb
ARJUNMASTER16 <anigam525@gmail.com>
2026-07-06 21:25:53 +0000

    updated

--- 02_basics/06_iife.js
+++ 02_basics/06_iife.js
@@ -11,3 +11,24 @@
     //simple IIFE
     console.log(`DB CONNECTED TWO ${name}`);
 } )('hitesh')
+
+
+
+
+
+//*******************javascript execution context******************* */
+
+/*
+{} => global E.C
+ => function E.C
+ => eval E.C
+
+ {}=>created in 2 phases 
+ 1.memory creation phase
+ 2.execution phase
+
+
+ new variable enviroment +execution thread
+
+ //watch at 6hrs5mins to recap
+*/
