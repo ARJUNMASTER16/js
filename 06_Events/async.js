184199623fbc32eecd50de979b5716ac212289ad
ARJUNMASTER16 <anigam525@gmail.com>
2026-07-19 20:51:41 +0000

    UPDATED

---
+++ 06_Events/async.js
@@ -0,0 +1,15 @@
+/*
+Javascript->synchronous & single thread                        (default)
+
+execution context->execute one line of code at a time
+(each operation waits for the last one to complete before executing)
+
+
+blocking code->block the flow of program->read file sync
+
+
+non-blocking code->does not block the execution->read file Async
+
+fetch()->promise->CB->high priority->stack
+
+*/
